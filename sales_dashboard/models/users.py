a5c9c389225da0c386b9e9bf79b79b94e7fb8121
= <remimanantsoa@gmail.com>
2022-06-07 11:16:01 +0300

    removed test in dashboard

--- sales_dashboard/models/users.py
+++ sales_dashboard/models/users.py
@@ -7,7 +7,6 @@ class ResUsers(models.Model):
 	_inherit = 'res.users'
 
 	customer_count = fields.Integer(string="Customer Count", compute="_count_customer", store=True)
-	clients = fields.Many2many('res.partner', string='Clients', compute="_count_customer", group_operator="count_distinct")
 
 	def _count_customer(self):
 		for record in self:
@@ -17,8 +16,4 @@ class ResUsers(models.Model):
 
 			customers = list(dict.fromkeys(customers))
 
-			record.clients = self.env['res.partner'].browse([customer.id for customer in customers])
-
 			record.customer_count = len(customers)
-
-			raise UserError("clients : %s\ncustomer_count: %s" % (record.clients, record.customer_count))
