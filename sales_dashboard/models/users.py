dcce048a2f7fae102d10680d6c152c0eba34435f
= <remimanantsoa@gmail.com>
2022-06-07 11:11:38 +0300

    updated dashboard module

--- sales_dashboard/models/users.py
+++ sales_dashboard/models/users.py
@@ -7,6 +7,7 @@ class ResUsers(models.Model):
 	_inherit = 'res.users'
 
 	customer_count = fields.Integer(string="Customer Count", compute="_count_customer", store=True)
+	clients = fields.Many2many('res.partner', string='Clients', compute="_count_customer", group_operator="count_distinct")
 
 	def _count_customer(self):
 		for record in self:
@@ -16,4 +17,8 @@ class ResUsers(models.Model):
 
 			customers = list(dict.fromkeys(customers))
 
+			record.clients = self.env['res.partner'].browse([customer.id for customer in customers])
+
 			record.customer_count = len(customers)
+
+			raise UserError("clients : %s\ncustomer_count: %s" % (record.clients, record.customer_count))
